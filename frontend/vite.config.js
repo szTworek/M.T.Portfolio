91b435c6e666406cbcacf7b046fcd50cf55195a3
Szymon <szymonntw@onet.pl>
2026-03-04 14:06:56 +0100

    deploy things

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/M.T.Portfolio',
   resolve: {
     alias: {
       'react': 'react',
